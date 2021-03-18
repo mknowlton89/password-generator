2f932286d4996f70cd4f406fc8c96c882a8e04b1
Michael Knowlton <mknowlton89@gmail.com>
2021-03-18 19:57:28 -0400

    Finalized the logic for the generator. Still need to handle the error state if someone enters a letter into the -how many characters would you like the pw to be field.

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,55 @@
 // Assignment Code
 const generateBtn = document.getElementById("generate");
 
-function generatePassword(){
-  let generatedPassword = "xYz123";
+function generatePassword(passwordLength, charPref){
+  
+  const lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  const uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] ;
+
+  const specialCharacters = ['?', '!', '@', '#', '$', '&', '%']
+
+  let charArray = [];
+
+  let pwArray = [];
+
+  let passLen = passwordLength;
+  let characterPreference = charPref;
+  // console.log("Password length is: " + passwordLength);
+  // console.log("Accept lowercase: " + characterPreference);
+
+  if (charPref.acceptLowercase) {
+    charArray = charArray.concat(lowercaseLetters);
+  }
+
+  if (charPref.acceptUppercase) {
+    charArray = charArray.concat(uppercaseLetters);
+  }
+
+  if (charPref.acceptNumbers) {
+    charArray = charArray.concat(numbers);
+  }
+
+  if (charPref.acceptSpecialCharacters) {
+    charArray = charArray.concat(specialCharacters);
+  }
+
+  for (i=0; i < passwordLength; i++) {
+    let randomNum = Math.floor(Math.random() * charArray.length);
+    
+    let randomChar = charArray[randomNum];
+
+    pwArray.push(randomChar);
+
+    console.log(pwArray);
+  }
+  
+  let generatedPassword = pwArray.join("");
+
+  console.log(generatedPassword);
+  
     return generatedPassword;
 }
 
@@ -10,6 +57,14 @@ function generatePassword(){
 function writePassword() {
   let passwordLength = prompt("How many characters would you like your password to be?");
 
+  let charPref = {
+    acceptLowercase: false,
+    acceptUppercase: false,
+    acceptNumbers: false,
+    acceptSpecialCharacters: false,
+  }
+
+  // Check to make sure the password length is valid
   if (passwordLength < 8 || passwordLength > 128 ) {
     alert("Password must be between 8 and 128 characters.");
     return 0;
@@ -17,18 +72,35 @@ function writePassword() {
 
   let acceptLowercase = confirm("Would you like your password to contain lowercase letters? \n \n Cancel = No \n Ok =  Yes");
 
+  if (acceptLowercase){
+    charPref.acceptLowercase = true;
+  }
+
   let acceptUppercase = confirm("Would you like your password to contain uppercase letters? \n \n Cancel = No \n Ok =  Yes");
+
+  if (acceptUppercase){
+    charPref.acceptUppercase = true;
+  }
 
   let acceptNumbers = confirm("Would you like your password to contain numbers? \n \n Cancel = No \n Ok =  Yes");
 
+  if (acceptNumbers){
+    charPref.acceptNumbers = true;
+  }
+
   let acceptSpecialCharacters = confirm("Would you like your password to contain special characters? EG: ! ? * & \n \n Cancel = No \n Ok =  Yes");
 
+  if (acceptSpecialCharacters){
+    charPref.acceptSpecialCharacters = true;
+  }
+
+  // Check to make sure atleast one of the character types is true.
   if (!acceptLowercase && !acceptNumbers && !acceptSpecialCharacters && !acceptUppercase) {
     alert("You must choose atleast one character type");
     return 0;
   }
 
-  let password = generatePassword();
+  let password = generatePassword(passwordLength, charPref);
 
   let passwordText = document.querySelector("#password");
 
@@ -59,11 +131,13 @@ generateBtn.addEventListener("click", writePassword);
 // An array for special characters 
 // const specialCharacters = ['?', '!', '@', '#', '$']
 
-// I'll need to create a for loop that 
+// I'll then need to create a new combined array (characters) that contains all of the characters the user has indicated that want in the password
 
-// I'll need to use math.floor.math.random to generate a random number and use that number to index into one of the arrays and select a character.
+// I'll then create a for loop that is as long as passwordLength
 
-// I'll need to add that character to the generatedPassword variable
+// Each loop through the loop will need to generate a random number (using math.floor.math(random) * length of the characters array) and use that number to index into the array to grab a character.
 
-// I'll need to do this until the length of characters === passwordLength
+// That character will be added to an array (passwordArray)
+
+// I'll then conver that passwordArray to a single string passwordArray.join("")
 
