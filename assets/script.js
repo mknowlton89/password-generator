432ba6778aa9dc63847f6335be4ff1e611e6842d
Michael Knowlton <mknowlton89@gmail.com>
2021-03-18 16:02:11 -0400

    Started building the logic for the user inputs.

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,34 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.getElementById("generate");
+
+function generatePassword(){
+  //TODO: 
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let passwordLength = prompt("How many characters would you like your password to be?");
 
-  passwordText.value = password;
+  console.log(passwordLength);
+
+  if (passwordLength < 8 || passwordLength > 128 ) {
+    alert("Password must be between 8 and 128 characters.");
+    return 0;
+  }
+
+  let acceptLowercase = confirm("Would you like your password to contain lowercase letters? \n \n Cancel = No \n Ok =  Yes");
+
+  let acceptUppercase = confirm("Would you like your password to contain uppercase letters? \n \n Cancel = No \n Ok =  Yes");
+
+  let acceptSpecialCharacters = confirm("Would you like your password to contain special characters? EG: ! ? * & \n \n Cancel = No \n Ok =  Yes");
+
+  // let password = generatePassword();
+  // let passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
