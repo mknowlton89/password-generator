c2d820cca4efe044da50adcee3806b2ee6c567f4
Michael Knowlton <mknowlton89@gmail.com>
2021-03-19 09:51:18 -0400

    Updated the readme and cleaned up the scripts.js file.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # password-generator
-This is a password generator, built with Javascript.
+This is a password generator, built with Javascript. A user can generate a password of a specific character length, and control if there are lowercase, uppercase, numbers, or special characters included in the password via a series of prompts.
+
+# Link to the Github repo
+https://github.com/mknowlton89/password-generator
+# Link to the deployed application
+https://mknowlton89.github.io/password-generator/
+
+# Screenshot of the deployed application
--- assets/script.js
+++ assets/script.js
@@ -4,21 +4,14 @@ const generateBtn = document.getElementById("generate");
 function generatePassword(passwordLength, charPref){
   
   const lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
   const uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
   const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] ;
-
   const specialCharacters = ['?', '!', '@', '#', '$', '&', '%']
 
   let charArray = [];
-
   let pwArray = [];
-
   let passLen = passwordLength;
   let characterPreference = charPref;
-  // console.log("Password length is: " + passwordLength);
-  // console.log("Accept lowercase: " + characterPreference);
 
   if (charPref.acceptLowercase) {
     charArray = charArray.concat(lowercaseLetters);
@@ -38,18 +31,11 @@ function generatePassword(passwordLength, charPref){
 
   for (i=0; i < passwordLength; i++) {
     let randomNum = Math.floor(Math.random() * charArray.length);
-    
     let randomChar = charArray[randomNum];
-
     pwArray.push(randomChar);
-
-    console.log(pwArray);
   }
   
   let generatedPassword = pwArray.join("");
-
-  console.log(generatedPassword);
-  
     return generatedPassword;
 }
 
